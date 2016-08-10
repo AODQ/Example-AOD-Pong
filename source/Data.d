1e9a3aeff42c71cc0400a3cb3d1ba1edcf2ab46f
ciggbit <hercludes@gmail.com>
2016-08-10 00:30:20 -0400

    added base for meteor images:

--- source/Data.d
+++ source/Data.d
@@ -3,9 +3,12 @@ static import AOD;
 
 class Image_Data {
 public: static:
-  AOD.SheetContainer boss;
+  AOD.SheetContainer meteor_large; // 64x64
+  AOD.SheetContainer[2] meteor_medium; // 32x32
+  AOD.SheetContainer[4] meteor_small; // 16x16
+  AOD.SheetContainer[8] meteor_tiny; // 8x8
 
   void Initialize() {
-    boss = AOD.SheetContainer("assets/debug-box.png");
+    // .. todo ..
   }
 }
