c3cb6bbe9bc517eedcba97db3b7ada79b0b1d4fa
ciggbit <hercludes@gmail.com>
2016-08-10 00:30:47 -0400

    paddle image

--- source/Data.d
+++ source/Data.d
@@ -3,6 +3,7 @@ static import AOD;
 
 class Image_Data {
 public: static:
+  AOD.SheetContainer paddle;
   AOD.SheetContainer meteor_large; // 64x64
   AOD.SheetContainer[2] meteor_medium; // 32x32
   AOD.SheetContainer[4] meteor_small; // 16x16
